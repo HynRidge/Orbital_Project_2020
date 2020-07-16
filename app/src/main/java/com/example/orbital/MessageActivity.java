848afb5e1cffe830a1c2bc4d08ce1c918fd6d0f9
Kacalamaka <heinrich2001.ang@gmail.com>
2020-07-16 18:25:53 +0800

    Updated MessageActivity.java

--- app/src/main/java/com/example/orbital/MessageActivity.java
+++ app/src/main/java/com/example/orbital/MessageActivity.java
@@ -47,6 +47,7 @@ public class MessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+        initialize();
     }
 
     @SuppressLint("RestrictedApi")
