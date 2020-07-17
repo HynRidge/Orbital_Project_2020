e1a01f89a1637865ec2e1291941e955acf1d5d2d
Kacalamaka <heinrich2001.ang@gmail.com>
2020-07-17 20:29:48 +0800

    Resolve problems in MessageActivity.java

--- app/src/main/java/com/example/orbital/MessageActivity.java
+++ app/src/main/java/com/example/orbital/MessageActivity.java
@@ -1,6 +1,3 @@
-
-package com.example.closefriendsapp;
-
 package com.example.orbital;
 
 
@@ -20,26 +17,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class MessageActivity extends AppCompatActivity {
-
-    private Toolbar chatToolBar;
-
-    private RecyclerView view;
-
-    private TextView name, typeMessageHint;
-
-    private ImageView profileImage;
-
-    private Button sendButton;
-
-    private MessageAdapter messageAdapter;
-
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -49,28 +26,22 @@ import com.example.orbital.Model.Message;
 import java.util.ArrayList;
 import java.util.List;
 
-import de.hdodenhof.circleimageview.CircleImageView;
-
 public class MessageActivity extends AppCompatActivity {
 
-     private Toolbar chatToolBar;
+    private Toolbar chatToolBar;
 
-     private RecyclerView recyclerView;
+    private RecyclerView recyclerView;
 
-     CircleImageView profile_image;
+    private TextView nickname;
 
-     private TextView nickname;
+    private ImageView profileImage;
 
-     private EditText text_send;
+    EditText edit_message;
 
-     private ImageView profileImage;
+    private Button sendButton;
 
-     private Button sendButton;
+    private MessageAdapter messageAdapter;
 
-     private MessageAdapter messageAdapter;
-
-
-    private final List<Message> messagesList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,24 +69,12 @@ public class MessageActivity extends AppCompatActivity {
         actionBar.setCustomView(actionBarView);
 
 
-        view = findViewById(R.id.rv_chats);
-
-        name = findViewById(R.id.username);
-
-        typeMessageHint = findViewById(R.id.edit_message);
-
-        profileImage = findViewById(R.id.custom_profile_image);
-
-        sendButton = findViewById(R.id.rounded_send_button);
-
         recyclerView = findViewById(R.id.message_recycler_view);
         recyclerView.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
         linearLayoutManager.setStackFromEnd(true);
 
         nickname = findViewById(R.id.username);
-
-        text_send = findViewById(R.id.edit_message);
 
         profileImage = findViewById(R.id.custom_profile_image);
 
