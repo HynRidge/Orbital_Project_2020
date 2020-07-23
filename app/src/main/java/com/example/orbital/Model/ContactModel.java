5b91549e68651fcbbe024cdc0ae7d45ae5cd1e99
Kacalamaka <heinrich2001.ang@gmail.com>
2020-07-23 16:20:43 +0800

    Updated ContactModel.java

--- app/src/main/java/com/example/orbital/Model/ContactModel.java
+++ app/src/main/java/com/example/orbital/Model/ContactModel.java
@@ -2,14 +2,14 @@ package com.example.orbital.Model;
 
 public class ContactModel {
     String contact_name;
-    int contact_id;
     int image;
 
-    public ContactModel(String contact_name, int contact_id, int image) {
+    public ContactModel(String contact_name, int image) {
         this.contact_name = contact_name;
-        this.contact_id = contact_id;
         this.image = image;
     }
+
+    public ContactModel(){}
 
     public String getContact_name() {
         return contact_name;
@@ -17,14 +17,6 @@ public class ContactModel {
 
     public void setContact_name(String contact_name) {
         this.contact_name = contact_name;
-    }
-
-    public int getContact_id() {
-        return contact_id;
-    }
-
-    public void setContact_id(int contact_id) {
-        this.contact_id = contact_id;
     }
 
     public int getImage() {
