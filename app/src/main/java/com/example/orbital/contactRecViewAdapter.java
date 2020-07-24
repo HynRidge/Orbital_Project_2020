9cf1764ef36f2b7f7a5e0542e1c228637122c3ab
Kacalamaka <heinrich2001.ang@gmail.com>
2020-07-24 16:43:21 +0800

    Update contactRecViewAdapter.java

--- app/src/main/java/com/example/orbital/contactRecViewAdapter.java
+++ app/src/main/java/com/example/orbital/contactRecViewAdapter.java
@@ -1,19 +1,35 @@
 package com.example.orbital;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.orbital.Model.ContactModel;
 import com.example.orbital.Model.chatModel;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.Inet4Address;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class contactRecViewAdapter extends RecyclerView.Adapter<contactRecViewAdapter.ViewHolder> {
 
@@ -21,6 +37,13 @@ public class contactRecViewAdapter extends RecyclerView.Adapter<contactRecViewAd
 
     Context mContext;
 
+    String BASE_URL = "http://172.31.123.43:8000/account/";
+    int CURRENT_USER_ID = LoginActivity.USER_ID;
+    int PRIVATE_CHAT = 1;
+
+    int ROOM_ID;
+
+    RequestQueue queue;
     public contactRecViewAdapter() {}
 
 
@@ -32,10 +55,113 @@ public class contactRecViewAdapter extends RecyclerView.Adapter<contactRecViewAd
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
+        queue = Volley.newRequestQueue(mContext);
 
         holder.contact_name.setText(contacts.get(position).getContact_name());
         holder.contact_profile_image.setImageResource(contacts.get(position).getImage());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkRoomCreated(position);
+
+                Intent intent = new Intent(mContext,MessageActivity.class);
+                intent.putExtra("contactId",contacts.get(position).getContact_id());
+                intent.putExtra("contactNickname",contacts.get(position).getContact_name());
+                intent.putExtra("contactImage",contacts.get(position).getImage());
+
+                mContext.startActivity(intent);
+            }
+        });
+    }
+
+    private void checkRoomCreated(final int position) {
+        String url = BASE_URL +"find-room/" + CURRENT_USER_ID + "/"  +contacts.get(position).getContact_id()+ "/";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
+                    if(jsonArray.isNull(0)) {
+                        createRoom(position);
+                    } else {
+                        JSONObject jsonObject =  jsonArray.getJSONObject(0);
+                        ROOM_ID = jsonObject.getInt("room");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(mContext, "Request to find room failed!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        queue.add(stringRequest);
+    }
+
+
+    private void createParticipants(int position) {
+        String url = BASE_URL +"participants/";
+        ArrayList<Integer> idList = new ArrayList<>();
+        idList.add(CURRENT_USER_ID);
+        idList.add(contacts.get(position).getContact_id());
+        for(final int id :idList) {
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    System.out.println("User " +id  + " added to participants");
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(mContext, "Try to create new participants but failed", Toast.LENGTH_SHORT).show();
+                }
+            }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("room", Integer.toString(ROOM_ID));
+                    params.put("user", Integer.toString(id));
+                    return params;
+                }
+            };
+            queue.add(stringRequest);
+        }
+    }
+
+    private void createRoom(final int position) {
+        String url = BASE_URL +"room/";
+        StringRequest stringRequest =  new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                System.out.println("Room Created");
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    ROOM_ID = jsonObject.getInt("id");
+                    System.out.println(ROOM_ID);
+                    createParticipants(position);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(mContext, "Try to create room but error", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params =  new HashMap<>();
+                params.put("name",CURRENT_USER_ID + " and " + contacts.get(position).getContact_id());
+                params.put("type",Integer.toString(PRIVATE_CHAT));
+                return params;
+            }
+        };
+        queue.add(stringRequest);
     }
 
     @Override
