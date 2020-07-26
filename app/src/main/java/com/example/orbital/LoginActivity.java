120a78f9bcdb9ccfa33f2aea20074ea93ad8797c
Kacalamaka <heinrich2001.ang@gmail.com>
2020-07-26 21:13:00 +0800

    Updated LoginActivity.java

--- app/src/main/java/com/example/orbital/LoginActivity.java
+++ app/src/main/java/com/example/orbital/LoginActivity.java
@@ -36,8 +36,8 @@ public class LoginActivity extends AppCompatActivity {
     TextView needAnAcc;
     RequestQueue queue;
     String USER_PHONE_NUMBER;
-    String LOGIN_URL ="http://172.31.123.43:8000/account/api/token/login/";
-    String BASE_URL = "http://172.31.123.43:8000/account/";
+    String LOGIN_URL ="http://172.31.123.95:8000/account/api/token/login/";
+    String BASE_URL = "http://172.31.123.95:8000/account/";
     public static int USER_ID;
     String accessToken,refreshToken;
 
