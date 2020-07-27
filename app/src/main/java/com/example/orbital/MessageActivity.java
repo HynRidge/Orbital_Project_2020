f99a8dd43a2d539305ee0697e808f0af610b9d0f
Kacalamaka <heinrich2001.ang@gmail.com>
2020-07-27 22:58:32 +0800

    Updated MessageActivity.java

--- app/src/main/java/com/example/orbital/MessageActivity.java
+++ app/src/main/java/com/example/orbital/MessageActivity.java
@@ -22,11 +22,25 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.orbital.Model.Message;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MessageActivity extends AppCompatActivity {
 
@@ -42,14 +56,24 @@ public class MessageActivity extends AppCompatActivity {
 
     ImageButton sendButton;
 
-    private MessageAdapter messageAdapter;
+
+    private MessageAdapter messageAdapter = new MessageAdapter();
 
     Intent intent;
 
     int CURRENT_USER_ID = LoginActivity.USER_ID;
+
     int CONTACT_ID;
     String CONTACT_NICKNAME;
     int CONTACT_IMG;
+    int ROOM_ID;
+
+
+    ArrayList<Message> msg;
+
+    String BASE_URL = "http://172.31.123.95:8000/account/";
+    RequestQueue queue;
+
 
 
     @Override
@@ -57,10 +81,73 @@ public class MessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
-
+        queue = Volley.newRequestQueue(getApplicationContext());
         initialize();
 
+        showMessages();
 
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateBackend();
+            }
+        });
+
+
+    }
+
+    private void updateBackend() {
+        String url = BASE_URL +"add-message/";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+               showMessages();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MessageActivity.this, "Failed to send message", Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("room",Integer.toString(ROOM_ID));
+                params.put("sender_user",Integer.toString(CURRENT_USER_ID));
+                params.put("receiver_user",Integer.toString(CONTACT_ID));
+                params.put("message",edit_message.getText().toString());
+                return params;
+            }
+        };
+        queue.add(stringRequest);
+    }
+
+    private void showMessages() {
+        String url = BASE_URL + "get-message/" + ROOM_ID +"/";
+        StringRequest stringRequest =  new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                msg.clear();
+                try {
+                    JSONArray jsonArray =  new JSONArray(response);
+                    for(int i = 0; i < jsonArray.length(); i++){
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        msg.add(new Message(jsonObject.getInt("sender_user"),ROOM_ID,jsonObject.getString("message")));
+                    }
+
+                    messageAdapter.setMessage(MessageActivity.this,msg);
+                    recyclerView.setAdapter(messageAdapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MessageActivity.this, "Failed to show messages", Toast.LENGTH_SHORT).show();
+            }
+        });
+        queue.add(stringRequest);
 
     }
 
@@ -101,11 +188,14 @@ public class MessageActivity extends AppCompatActivity {
 
 
         intent = getIntent();
-        CONTACT_ID = intent.getIntExtra("contactId",0);
+        CONTACT_ID = intent.getIntExtra("contactId", -1);
         CONTACT_NICKNAME = intent.getStringExtra("contactNickname");
         CONTACT_IMG = intent.getIntExtra("contactImage", R.drawable.defaultpic);
+        ROOM_ID = intent.getIntExtra("roomID", -1);
 
         nickname.setText(CONTACT_NICKNAME);
         profileImage.setImageResource(CONTACT_IMG);
+
+        msg = new ArrayList<Message>();
     }
 }
