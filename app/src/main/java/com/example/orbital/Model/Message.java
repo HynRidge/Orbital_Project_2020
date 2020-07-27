257fe2378c58910d6d67b517dad3e472ee977084
Kacalamaka <heinrich2001.ang@gmail.com>
2020-07-27 22:57:12 +0800

    Updated Message.java

--- app/src/main/java/com/example/orbital/Model/Message.java
+++ app/src/main/java/com/example/orbital/Model/Message.java
@@ -2,13 +2,11 @@ package com.example.orbital.Model;
 
 public class Message {
         private int senderID;
-        private int receiverID;
         private int roomID;
         private String message;
 
-    public Message(int senderID, int receiverID, int roomID, String message) {
+    public Message(int senderID, int roomID, String message) {
         this.senderID = senderID;
-        this.receiverID = receiverID;
         this.roomID = roomID;
         this.message = message;
     }
@@ -19,14 +17,6 @@ public class Message {
 
     public void setSenderID(int senderID) {
         this.senderID = senderID;
-    }
-
-    public int getReceiverID() {
-        return receiverID;
-    }
-
-    public void setReceiverID(int receiverID) {
-        this.receiverID = receiverID;
     }
 
     public int getRoomID() {
