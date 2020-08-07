1499d5ea67dce584cfad1ea9c99ba9aa6cf69b30
Kacalamaka <heinrich2001.ang@gmail.com>
2020-08-07 14:57:12 +0800

    Changing IP Address

--- app/src/main/java/com/example/orbital/Model/URL.java
+++ app/src/main/java/com/example/orbital/Model/URL.java
@@ -1,5 +1,5 @@
 package com.example.orbital.Model;
 
 public interface URL {
-    String BASE_URL ="http://172.31.121.244:8000/account/";
+    String BASE_URL ="http://172.31.120.31:8000/account/";
 }
